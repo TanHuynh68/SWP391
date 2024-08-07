2c21d3d10fa8095d109b73b02c779a1ed8d73750
NguyenHovan <hovannguyenvl@gmail.com>
2024-08-07 07:41:37 +0700

    .

--- FE SWP/src/App.tsx
+++ FE SWP/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './router/AppRouter';
-
+import AppRouter from "./router/AppRouter"
 
 const App: React.FC = () => {
   return (
     <Router>
-      <AppRouter />
+      <AppRouter/>
     </Router>
   );
 };
--- FE SWP/src/router/AppRouter.tsx
+++ FE SWP/src/router/AppRouter.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import { paths } from "../constants";
+// import { Routes, Route } from "react-router-dom";
+
 import { Routes, Route } from "react-router-dom";
-import { paths } from "../constants";
-import {Home} from "../pages";
+import { lazy } from "react";
+const Home = lazy(() => import("@pages/home/index"));
 import Login from "@/pages/Login";
-const AppRouter: React.FC = () => {
-    return (
-        <Routes>
-            {/* Route for Guest */}
-            <Route path={paths.HOME} element={<Home/>} />
-            <Route path={paths.LOGIN} element={<Login/>} />
-            {/* <Route path={paths.ABOUT} element={<About />} /> */}
-        </Routes>
-    )
-}
-export default AppRouter;
+import { RouteObject } from "react-router-dom";
+
+export const NormalRouters: RouteObject[] = [ 
+    {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+]
+
+
+
+
+
+
+// const AppRouter: React.FC = () => {
+//     return (
+//         <Routes>
+//             {/* Route for Guest */}
+//             <Route path={paths.HOME} element={<Home/>} />
+//             <Route path={paths.LOGIN} element={<Login/>} />
+
+//             {/* <Route path={paths.ABOUT} element={<About />} /> */}
+//         </Routes>
+//     )
+// }
+// export default AppRouter;
