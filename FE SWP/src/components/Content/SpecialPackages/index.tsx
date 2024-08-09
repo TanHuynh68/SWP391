6ddf9fb83131c4b4288bb1900cbcafe96243ec56
NguyenHovan <hovannguyenvl@gmail.com>
2024-08-09 14:07:29 +0700

    UP:: update scpeccial

---
+++ FE SWP/src/components/Content/SpecialPackages/index.tsx
@@ -0,0 +1,19 @@
+import { specialPackagesData } from '@data/specialPackagesData';
+import styles from './SpecialPackages.module.css';
+
+const SpecialPackages = () => {
+    return (
+        <div className={styles.container}>
+            {specialPackagesData.slice(0, 4).map((packageItem, index) => (
+                <div key={index} className={styles.packageItem}>
+                    <img src={packageItem.imageUrl} alt={packageItem.title} className={styles.image} />
+                    <h3 className={styles.title}>{packageItem.title}</h3>
+                    <p className={styles.description}>{packageItem.description}</p>
+                    <a href={packageItem.link} className={styles.link}>XEM THÊM</a>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default SpecialPackages;
---
+++ FE SWP/src/data/specialPackagesData.ts
@@ -0,0 +1,78 @@
+import img1 from "@assets/fetch-data/special/image1.png"
+import img2 from "@assets/fetch-data/special/image2.png"
+import img3 from "@assets/fetch-data/special/image3.png"
+
+export const specialPackagesData = [
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+    {
+        title: 'Gói chăm sóc răng 8020 tại Nha khoa Đại Dương',
+        description: 'Với Gói chăm sóc răng 8020, cả gia đình sẽ được theo dõi sức khỏe hàm răng một cách toàn diện.',
+        link: '#',
+        imageUrl: img2,
+    },
+    {
+        title: 'Ưu đãi 30% Chi phí trồng răng Implant tại Nha khoa Trồng răng Sài Gòn',
+        description: 'Nha khoa Trồng răng Sài Gòn là địa chỉ trồng răng Implant tin cậy tại TPHCM.',
+        link: '#',
+        imageUrl: img3,
+    },
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+    {
+        title: 'Gói chăm sóc răng 8020 tại Nha khoa Đại Dương',
+        description: 'Với Gói chăm sóc răng 8020, cả gia đình sẽ được theo dõi sức khỏe hàm răng một cách toàn diện.',
+        link: '#',
+        imageUrl: img2,
+    },
+    {
+        title: 'Ưu đãi 30% Chi phí trồng răng Implant tại Nha khoa Trồng răng Sài Gòn',
+        description: 'Nha khoa Trồng răng Sài Gòn là địa chỉ trồng răng Implant tin cậy tại TPHCM.',
+        link: '#',
+        imageUrl: img3,
+    },
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+    {
+        title: 'Gói chăm sóc răng 8020 tại Nha khoa Đại Dương',
+        description: 'Với Gói chăm sóc răng 8020, cả gia đình sẽ được theo dõi sức khỏe hàm răng một cách toàn diện.',
+        link: '#',
+        imageUrl: img2,
+    },
+    {
+        title: 'Ưu đãi 30% Chi phí trồng răng Implant tại Nha khoa Trồng răng Sài Gòn',
+        description: 'Nha khoa Trồng răng Sài Gòn là địa chỉ trồng răng Implant tin cậy tại TPHCM.',
+        link: '#',
+        imageUrl: img3,
+    },
+    {
+        title: 'Ưu đãi 20% chi phí Niềng răng tại Nha khoa Lạc Việt (HN)',
+        description: 'Nha khoa Lạc Việt là địa chỉ niềng răng tin cậy, ứng dụng nhiều công nghệ mới và hỗ trợ nâng cao hiệu quả của quá trình niềng răng cho khách hàng',
+        link: '#',
+        imageUrl: img1,
+    },
+];
