5494494274e4707d2418b184de5cd97ea7916f6a
NguyenHovan <hovannguyenvl@gmail.com>
2024-08-19 09:21:21 +0700

    carousel doctor

--- FE SWP/src/components/Content/Doctor/index.tsx
+++ FE SWP/src/components/Content/Doctor/index.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDoctors } from '@redux/Slice/doctorSlice';
 import { RootState, AppDispatch } from '@redux/store/store';
+import { useSpring, animated } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
 import styles from './Doctor.module.css';
 
 const Doctor: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const doctors = useSelector((state: RootState) => state.doctor.doctors);
+  const [index, setIndex] = useState(0); 
+  const [props, set] = useSpring(() => ({
+    x: 0,
+  }));
 
   useEffect(() => {
     dispatch(fetchDoctors());
@@ -19,21 +25,39 @@ const Doctor: React.FC = () => {
     </div>
   );
 
+  
+  const bind = useDrag(({ direction: [xDir], distance, cancel }) => {
+    if (distance > window.innerWidth / 4) cancel(); 
+
+    if (xDir < 0 && index < doctors.length - 4) {
+      
+      setIndex(index + 1);
+    } else if (xDir > 0 && index > 0) {
+      
+      setIndex(index - 1);
+    }
+
+    
+    set({
+      x: -index * (window.innerWidth / 4),
+    });
+  });
+
   return (
     <div className={styles.doctorContainer}>
       <div className={styles.head}>
         <h2>Nha sĩ nổi bật</h2>
         <button className={styles.allPostsButton}>
-        {/* onClick={() => navigate('/doctors')} */}
+          {/* onClick={() => navigate('/doctors')} */}
           Tìm kiếm
         </button>
       </div>
       <div className={styles.carouselContainer}>
-        <div className={styles.doctorList}>
+        <animated.div {...bind()} className={styles.doctorList} style={{ x: props.x }}>
           {doctors.map((doctor, i) => (
             <DoctorCard key={i} name={doctor.account.fullName} imageUrl={doctor.image} />
           ))}
-        </div>
+        </animated.div>
       </div>
     </div>
   );
